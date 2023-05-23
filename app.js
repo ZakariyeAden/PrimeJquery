d9eb443be4c6c2c48b9b1f49c4098be834dd7d64
ZakariyeAden <zakariye5417@gmail.com>
2023-05-23 13:46:03 -0500

    update

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ function readyNow(){
   $('#sayHelloBtn').on('click', sayHello);
 }
 
+function sayHello(){
+  // Target the input element
+  // get the value of text input
+  let userName = $('#nameInput').val();
+  console.log(`Hello ${userName}`);
+  
+  // Empy the value
+  userName = $('nameInput').val('');
+
+}
